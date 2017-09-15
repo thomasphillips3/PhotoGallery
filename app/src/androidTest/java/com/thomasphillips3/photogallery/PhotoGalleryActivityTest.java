864001ae3b6b0a29be10d560f5217374fd00c0c3
Thomas Phillips <thomasphillips3@gmail.com>
2017-09-14 23:03:34 -0700

    Added .idea folder.

---
+++ app/src/androidTest/java/com/thomasphillips3/photogallery/PhotoGalleryActivityTest.java
@@ -0,0 +1,108 @@
+package com.thomasphillips3.photogallery;
+
+
+import android.support.test.espresso.DataInteraction;
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.pressBack;
+import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
+import static android.support.test.espresso.action.ViewActions.*;
+import static android.support.test.espresso.assertion.ViewAssertions.*;
+import static android.support.test.espresso.matcher.ViewMatchers.*;
+
+import com.thomasphillips3.photogallery.R;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.anything;
+import static org.hamcrest.Matchers.is;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class PhotoGalleryActivityTest {
+
+    @Rule
+    public ActivityTestRule<PhotoGalleryActivity> mActivityTestRule = new ActivityTestRule<>(PhotoGalleryActivity.class);
+
+    @Test
+    public void photoGalleryActivityTest() {
+        ViewInteraction textView = onView(
+allOf(withText("PhotoGallery"),
+childAtPosition(
+allOf(withId(R.id.action_bar),
+childAtPosition(
+withId(R.id.action_bar_container),
+0)),
+0),
+isDisplayed()));
+        textView.check(matches(withText("PhotoGallery")));
+        
+        ViewInteraction imageView = onView(
+allOf(withId(R.id.search_button), withContentDescription("Search"),
+childAtPosition(
+allOf(withId(R.id.search_bar),
+childAtPosition(
+withId(R.id.menu_item_search),
+0)),
+0),
+isDisplayed()));
+        imageView.check(matches(isDisplayed()));
+        
+        ViewInteraction textView2 = onView(
+allOf(withId(R.id.menu_item_toggle_polling), withText("START POLLING"), withContentDescription("Start Polling"),
+childAtPosition(
+childAtPosition(
+withId(R.id.action_bar),
+1),
+1),
+isDisplayed()));
+        textView2.check(matches(withText("START POLLING")));
+        
+        ViewInteraction frameLayout = onView(
+allOf(withId(R.id.fragment_container),
+childAtPosition(
+allOf(withId(android.R.id.content),
+childAtPosition(
+withId(R.id.decor_content_parent),
+1)),
+0),
+isDisplayed()));
+        frameLayout.check(matches(isDisplayed()));
+        
+        }
+
+        private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup)parent).getChildAt(position));
+            }
+        };
+    }
+    }
